02c88ae26b99b6fba80312ea35b283985dcc7148
Shaygi <104856049+Shaygi@users.noreply.github.com>
2023-07-03 17:56:35 +0200

    Tierkarten hinzugefügt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import {HomeComponent} from "./home/home.component";
 import {VermittlungComponent} from "./vermittlung/vermittlung.component";
 import {ContactComponent} from "./contact/contact.component";
 import {SpendenComponent} from "./spenden/spenden.component";
+import {KatzeComponent} from "./katze/katze.component";
+import {HundComponent} from "./hund/hund.component";
+import {KleintierComponent} from "./kleintier/kleintier.component";
+import {CardComponent} from "./card/card.component";
 
 const routes: Routes = [
   {path: '', component:HomeComponent },
   {path: 'vermittlung', component: VermittlungComponent},
   {path: 'contact', component: ContactComponent},
-  {path: 'spenden', component: SpendenComponent}
+  {path: 'spenden', component: SpendenComponent},
+  {path: 'katze', component: KatzeComponent},
+  {path: 'hund', component: HundComponent},
+  {path: 'kleintier', component: KleintierComponent},
+  {path: 'card', component: CardComponent}
 
 ];
 
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,27 +1,31 @@
 import {Component, Input} from '@angular/core';
-
-
+import { OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
+
 export class CardComponent {
   @Input() tierart: string;
   @Input() beschreibung: string;
   @Input() bild: string;
   @Input() groesse: string;
-  @Input() link: string;
+  @Input() weblink: string;
+  @Input() filter: string;
+  @Input() routerLink: string;
 
-  constructor() {
+  constructor(private route: ActivatedRoute) {
     this.tierart = 'Card Title';
     this.beschreibung = 'Tier Info';
     this.bild = 'path/to/default-image.jpg';
     this.groesse = 'bildgroesse';
-    this.link = 'link';
+    this.weblink = 'seitenlink';
+    this.filter = 'filter';
+    this.routerLink = 'filterLink';
+
   }
-
-
 
 }
 
