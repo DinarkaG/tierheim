6a9e214fe7b4fe4b4e9a94881f42bee66b99c770
snb02 <104787845+snb02@users.noreply.github.com>
2023-07-07 21:57:47 +0200

    newsletter

--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {NewsletterService} from '../newsletter.service';
 
 @Component({
   selector: 'app-impressum',
   templateUrl: './impressum.component.html',
-  styleUrls: ['./impressum.component.css']
+  styleUrls: ['./impressum.component.css'],
+  providers: [NewsletterService]
 })
-export class ImpressumComponent {
 
+export class ImpressumComponent implements OnInit {
+  email: string = ''; // Variable zum Speichern der E-Mail-Adresse
+
+  constructor(private http: HttpClient, private newsletterService: NewsletterService) {}
+
+  ngOnInit() {
+  }
+  addtoEmail(email: string) {
+    const url = '/api/addemail';
+    const body = { email };
+
+    this.http.post(url, body).subscribe(
+      (response) => {
+        console.log('Data added successfully');
+      },
+      (error) => {
+        console.error('Error adding data:', error);
+      }
+    );
+  }
 }
+
+
+
+
+
